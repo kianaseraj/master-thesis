945f803afa47542f98d1f5daa15ba2074f03a583
Kiana <kianaseraj98@gmail.com>
2024-07-01 18:36:50 +0200

    Define ASA_pooling object

--- pooling.py
+++ pooling.py
@@ -1,52 +1,61 @@
+
 import torch
-import torch.nn as nn
-from constants import *
-from torch.autograd import Variable
-from Bio.PDB.SASA import ShrakeRupley
-from Bio.PDB import PDBParser
-from dataclasses import dataclass
+from typing import List
 from abc import abstractmethod, ABC
 import numpy as np
-import os
 import ast
 
-#Amino acids having less than 30% of relative ASA will be eliminated from feature embeddings.
 
-"""
-based on the dssp putput:
-(dssp index, amino acid, secondary structure, relative ASA, phi, psi,
-NH_O_1_relidx, NH_O_1_energy, O_NH_1_relidx, O_NH_1_energy,
-NH_O_2_relidx, NH_O_2_energy, O_NH_2_relidx, O_NH_2_energy)
-"""
-dir_path = "/Users/kianaseraj/Desktop/github-kianaseraj/master-thesis/dataset/dssp_go"
-os.chdir(dir_path)
+"""Define pooling object consisting of pooling methods : mean, max, ASA, etc."""
+
+class pooling(ABC):
+  def __init__(self):
+    pass
+  
+  @abstractmethod
+  def embedding_pooling(self):
+    pass
+  
+    
 def dssp_get(dssp_file):
-    prot_id = os.path.splitext(dssp_file)[0]
+    """read dssp file"""
+    prot_id = dssp_file.split("/")[-1].split(".")[0]
     dssp_output = np.load(dssp_file)
     dssp_output = dssp_output.item()
     dssp_output = ast.literal_eval(dssp_output)
     return dssp_output, prot_id
 
-def accessible_residues(dssp_output):
-    
+def accessible_residues(dssp_output) -> List:
+    """based on the dssp values, residues having less than 0.3 relative accessible surface area will be eliminated from the list of indices"""
     surface_accessible_aa = []
     for residue_index, dssp_val in enumerate(dssp_output):
-        if dssp_val[3] >= 0.3:
+        if float(dssp_val[3]) >= 0.3:
             surface_accessible_aa.append(residue_index)
     return surface_accessible_aa
 
-#storing all sequences with their surface-accessible residues in a dict format
-ASA_dict = {}
-for file in os.listdir(dir_path):
-    dssp_output, prot_id = dssp_get(file)
+def sequence_embedding(embedding_file, sequence_file):
+  """the esm token representation inserts 0 at the beginning and 2 at the end of each sequence.Additionally, to pad sequences to the max sequence length, number 1 will be added at the end after token id 2!
+  example : "ATIDPK"=[0, 5, 11, 12, 13, 14, 15, 2, 1, 1, 1, 1]
+  Hence, check if the embedding's length matches the length of its sequence, otherwise, eliminated the padding tokens!""" 
+  embedding = torch.load(embedding_file)
+  with open(sequence_file, "r") as f:
+    sequence = f.read()
+  embedding = embedding[0][1:len(sequence)+1]
+  return embedding
+
+
+class ASA_pooling():
+  def __init__(self):
+    super().__init__()
+    
+  def embedding_pooling(dssp_file, embedding_file : str, sequence_file : str, save_path : str) ->torch.Tensor:
+    """
+    Amino acids having less than 30% of relative ASA will be eliminated from feature embeddings.
+    """
+    dssp_output, prot_id = dssp_get(dssp_file)
     surface_accessible_aa = accessible_residues(dssp_output)
-    ASA_dict[prot_id] = surface_accessible_aa
-    
-
-    
-
-        
-    
-
+    embedding = sequence_embedding(embedding_file, sequence_file)
+    embedding = embedding[surface_accessible_aa]
+    torch.save(embedding, save_path+"/"+f"{prot_id}.pt")
 
     
